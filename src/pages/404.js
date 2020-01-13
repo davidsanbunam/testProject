2849c73fecaac1e3de22399038b9331a9ffe2991
sanbunam <softdavidlee@gmail.com>
2020-01-12 23:24:02 -0800

    test2

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
-    <h2>You just hit a route that doesn&#39;t exist.</h2>
+    <h2>You just hit a route that doesn&#39;t exist..</h2>
   </Layout>
 );
 
